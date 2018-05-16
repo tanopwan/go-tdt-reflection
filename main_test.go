f55c61ac2057a7dec0da1e21ebc846e03f64fb18
Tanopwan Tangprakit <tanopwan@gmail.com>
2018-05-16 18:17:05 +0700

    Passing root element value

--- main_test.go
+++ main_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 
 	m "github.com/tanopwan/go-tdt-reflection"
@@ -22,7 +24,7 @@ var testDomains = []testDomain{
 	}), "header", "/path/to/resource1"},
 	{reflect.ValueOf(&m.MyShape{
 		Shapes: []m.Shape{m.Shape{Color: "Blue", Edge: 3}, m.Shape{Color: "Purple", Edge: 2}},
-		Shape:  m.Shape{},
+		Shape:  m.Shape{Color: "Pink", Edge: 11},
 	}), "header", "/path/to/resource2"},
 }
 
@@ -36,62 +38,81 @@ func TestDomains(t *testing.T) {
 	for _, tt := range testDomains {
 		indirect := tt.requestDomain
 		t.Run(indirect.Elem().Type().Name(), func(t *testing.T) {
-			dive(t, "", indirect.Elem())
+			dive(t, "object", indirect.Elem(), indirect.Elem())
 		})
 	}
 }
 
-func dive(t *testing.T, path string, indirectElem reflect.Value) {
+func dive(t *testing.T, path string, indirectElem reflect.Value, root reflect.Value) {
 	for i := 0; i < indirectElem.NumField(); i++ {
 		newElem := reflect.New(indirectElem.Type())
 		newElem.Elem().Set(reflect.ValueOf(indirectElem.Interface()))
-		fmt.Printf(">>> newElem: %v, type: %s, no: %d\n", newElem, newElem.Type(), newElem.Elem().NumField())
+		fmt.Printf("[%s]-----------------------------------------\n", path)
+		fmt.Printf("[%s]newElem: %v, type: %s, no: %d\n", path, newElem, newElem.Type(), newElem.Elem().NumField())
 		elem := newElem.Elem()
 
 		t.Run(elem.Type().Field(i).Name, func(t *testing.T) {
-			fmt.Printf("%sfield type: %v, kind: %s\n", path, elem.Field(i).Type(), elem.Field(i).Kind())
-			fmt.Printf("%sstruct field: %v, Tag: %s\n", path, elem.Type().Field(i), elem.Type().Field(i).Tag)
+			fmt.Printf("[%s]field type: %v, kind: %s\n", path, elem.Field(i).Type(), elem.Field(i).Kind())
+			fmt.Printf("[%s]struct field: %v, Tag: %s\n", path, elem.Type().Field(i), elem.Type().Field(i).Tag)
 			if elem.Field(i).Kind() == reflect.Struct {
-				dive(t, path+"...", elem.Field(i))
+				fmt.Printf("[%s]Found struct dive\n", path)
+				dive(t, path+"."+elem.Type().Field(i).Name, elem.Field(i), root)
 			} else if elem.Field(i).Kind() == reflect.Slice {
+				fmt.Printf("[%s]Found slice dive\n", path)
 				for ii := 0; ii < elem.Field(i).Len(); ii++ {
-
-					indirectFieldIndex := elem.Field(i).Index(ii)
-					dive(t, fmt.Sprintf("%s...[%d]", path, ii), indirectFieldIndex)
+					newField := reflect.New(elem.Field(i).Index(ii).Type())
+					newField.Elem().Set(reflect.ValueOf(elem.Field(i).Index(ii).Interface()))
+					dive(t, fmt.Sprintf("%s.%s.%d", path, elem.Type().Field(i).Name, ii), newField.Elem(), root)
 				}
 			} else if elem.Field(i).Kind() == reflect.String {
-				fmt.Printf("%s[Test] String\n", path)
-				callTest(t, path, elem.Field(i).Addr())
-
-				payload, err := json.Marshal(elem.Interface())
-				if err != nil {
-					panic(err)
-				}
-				fmt.Printf("%s[Test]... request payload: %s\n", path, string(payload))
+				fmt.Printf("[%s][Test] String\n", path)
+				elem.Field(i).SetString("")
 			} else if elem.Field(i).Kind() == reflect.Int {
-				fmt.Printf("%s[Test] Int\n", path)
-				callTest(t, path, elem.Field(i).Addr())
-
-				payload, err := json.Marshal(elem.Interface())
-				if err != nil {
-					panic(err)
-				}
-				fmt.Printf("%s[Test]... request payload: %s\n", path, string(payload))
+				fmt.Printf("[%s][Test] Int\n", path)
+				elem.Field(i).SetInt(0)
 			} else {
-				fmt.Printf("%s[Test] Unknown\n", path)
+				fmt.Printf("[%s][Test] Unknown\n", path)
+				return
 			}
-		})
-	}
-}
 
-func callTest(t *testing.T, path string, indirect reflect.Value) {
-	if indirect.Elem().Kind() == reflect.String {
-		indirect.Elem().SetString("")
-	} else if indirect.Elem().Kind() == reflect.Slice {
-		indirect.Elem().SetLen(0)
-	} else if indirect.Elem().Kind() == reflect.Int {
-		indirect.Elem().SetInt(0)
-	} else {
-		fmt.Printf("%s[Test] ... skip field unknown Kind: %v\n", path, indirect.Elem().Kind())
+			fmt.Printf("[%s]testElem: %v, type: %s\n", path, elem, newElem.Type())
+
+			newRoot := reflect.New(root.Type())
+			newRoot.Elem().Set(reflect.ValueOf(root.Interface()))
+			cn := newRoot.Elem()
+			currentNode := &cn
+			nodes := strings.Split(path, ".")
+			if len(nodes) == 1 {
+				newRoot = elem
+			} else {
+
+				for index, node := range nodes {
+					if node == "object" {
+						continue
+					}
+
+					if index >= len(nodes)-2 {
+						fmt.Printf("[%s]found at node: %s\n", path, node)
+						if currentNode.FieldByName(node).Kind() == reflect.Slice {
+							n, _ := strconv.Atoi(nodes[index+1])
+							currentNode.FieldByName(node).Index(n).Set(elem)
+						} else {
+							currentNode.FieldByName(node).Set(elem)
+						}
+						break
+					}
+
+					fmt.Printf("[%s]currentNode: %v, type: %s at index %d/%d\n", path, currentNode, currentNode.Type(), index, len(nodes)-1)
+					newCurrentNode := currentNode.FieldByName(node)
+					currentNode = &newCurrentNode
+				}
+			}
+
+			payload, err := json.Marshal(newRoot.Interface())
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("[%s][Test] request payload: %s\n", path, string(payload))
+		})
 	}
 }
