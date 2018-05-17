ef10a95079d4afac63d1e9ed936af43311e1109d
Tanopwan Tangprakit <tanopwan@gmail.com>
2018-05-17 17:51:54 +0700

    Create new type walker

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
+var domains = []Domain{
+	{
+		Shape{
+			Color: "Yellow",
+			Edge:  4,
+		},
+	},
+	{
+		MyShape{
+			Foos:  []Foo{{FooString: "foo1", Another: 1, Barfs: []Bar{{Barf: "B1"}}}, {FooString: "foo2", Another: 2}},
+			Shape: Shape{Color: "Pink", Edge: 11},
+		},
+	},
+}
+
 func main() {
-	listStruct()
-	listValue()
+	w := NewWalker()
+	w.Init(domains)
 }
--- walk_type.go
+++ walk_type.go
@@ -5,54 +5,58 @@ import (
 	"reflect"
 )
 
-type domain struct {
+// Domain ...
+type Domain struct {
 	d interface{}
 }
 
-var domains = []domain{
-	{
-		Shape{
-			Color: "Yellow",
-			Edge:  4,
-		},
-	},
-	{
-		MyShape{
-			Foos:  []Foo{{FooString: "foo1", Another: 1, Barfs: []Bar{{Barf: "B1"}}}, {FooString: "foo2", Another: 2}},
-			Shape: Shape{Color: "Pink", Edge: 11},
-		},
-	},
+// Walker ...
+type Walker interface {
+	Init(d []Domain)
 }
 
-var paths []string
+type walker struct {
+	domains []Domain
+	paths   []string
+}
 
-func listStruct() {
-	for _, domain := range domains {
+// NewWalker ...
+func NewWalker() Walker {
+	return &walker{}
+}
+
+func (w *walker) Init(d []Domain) {
+	w.domains = d
+	w.listStruct()
+}
+
+func (w *walker) listStruct() {
+	for _, domain := range w.domains {
 		typeOf := reflect.TypeOf(domain.d)
-		dive(typeOf, typeOf.Name())
+		w.walkStruct(typeOf, typeOf.Name())
 	}
 }
 
-func dive(tt reflect.Type, path string) {
+func (w *walker) walkStruct(tt reflect.Type, path string) {
 	for i := 0; i < tt.NumField(); i++ {
 		f := tt.Field(i)
 		switch f.Type.Kind() {
 		case reflect.Struct:
 			fmt.Printf("[struct] path: %s\n", fmt.Sprintf("%s.%s", path, f.Type.Name()))
-			dive(f.Type, fmt.Sprintf("%s.%s", path, f.Type.Name()))
+			w.walkStruct(f.Type, fmt.Sprintf("%s.%s", path, f.Type.Name()))
 		case reflect.Slice:
 			fmt.Printf("[slice ] path: %s\n", fmt.Sprintf("%s.[]%s", path, f.Type.Elem().Name()))
-			dive(f.Type.Elem(), fmt.Sprintf("%s.[]%s", path, f.Type.Elem().Name()))
+			w.walkStruct(f.Type.Elem(), fmt.Sprintf("%s.[]%s", path, f.Type.Elem().Name()))
 		case reflect.String:
 			res := fmt.Sprintf("%s.%s", path, f.Name)
 			fmt.Printf("[string] path: %s\n", res)
 
-			paths = append(paths, res)
+			w.paths = append(w.paths, res)
 			continue
 		case reflect.Int:
 			res := fmt.Sprintf("%s.%s", path, f.Name)
 			fmt.Printf("[int   ] path: %s\n", res)
-			paths = append(paths, res)
+			w.paths = append(w.paths, res)
 			continue
 		default:
 			fmt.Printf("[unknown] path: %s\n", fmt.Sprintf("%s.%s", path, f.Name))
