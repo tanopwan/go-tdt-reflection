f2a98ab7db03e0126369783a5ec54adf69a3346f
Tanopwan Tangprakit <tanopwan@gmail.com>
2018-06-08 11:00:53 +0700

    Temp

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 var domains = []Domain{
 	{
 		Shape{
@@ -16,6 +21,24 @@ var domains = []Domain{
 }
 
 func main() {
+
+	type embeded struct {
+		data interface{}
+	}
+
+	s := Shape{
+		Color: "T",
+		Edge:  1,
+	}
+
+	e := embeded{
+		data: s,
+	}
+	v := reflect.ValueOf(&e.data)
+	i := v.Elem()
+
+	fmt.Printf("value of domain: %v, with type: %v\n", i, reflect.TypeOf(i.Interface()).Name())
+
 	w := NewWalker()
 	w.Init(domains)
 }
--- walk_type.go
+++ walk_type.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strconv"
+	"strings"
 )
 
 // Domain ...
@@ -18,6 +20,14 @@ type Walker interface {
 type walker struct {
 	domains []Domain
 	paths   []string
+	results []ResultRow
+}
+
+// ResultRow ...
+type ResultRow struct {
+	path string
+	body string
+	tag  string
 }
 
 // NewWalker ...
@@ -28,12 +38,13 @@ func NewWalker() Walker {
 func (w *walker) Init(d []Domain) {
 	w.domains = d
 	w.listStruct()
+	w.generateTable()
 }
 
 func (w *walker) listStruct() {
-	for _, domain := range w.domains {
+	for i, domain := range w.domains {
 		typeOf := reflect.TypeOf(domain.d)
-		w.walkStruct(typeOf, typeOf.Name())
+		w.walkStruct(typeOf, fmt.Sprintf("%d.%s", i, typeOf.Name()))
 	}
 }
 
@@ -42,11 +53,11 @@ func (w *walker) walkStruct(tt reflect.Type, path string) {
 		f := tt.Field(i)
 		switch f.Type.Kind() {
 		case reflect.Struct:
-			fmt.Printf("[struct] path: %s\n", fmt.Sprintf("%s.%s", path, f.Type.Name()))
-			w.walkStruct(f.Type, fmt.Sprintf("%s.%s", path, f.Type.Name()))
+			fmt.Printf("[struct] path: %s\n", fmt.Sprintf("%s.%s", path, f.Name))
+			w.walkStruct(f.Type, fmt.Sprintf("%s.%s", path, f.Name))
 		case reflect.Slice:
-			fmt.Printf("[slice ] path: %s\n", fmt.Sprintf("%s.[]%s", path, f.Type.Elem().Name()))
-			w.walkStruct(f.Type.Elem(), fmt.Sprintf("%s.[]%s", path, f.Type.Elem().Name()))
+			fmt.Printf("[slice ] path: %s\n", fmt.Sprintf("%s.[]%s", path, f.Name))
+			w.walkStruct(f.Type.Elem(), fmt.Sprintf("%s.[]%s", path, f.Name))
 		case reflect.String:
 			res := fmt.Sprintf("%s.%s", path, f.Name)
 			fmt.Printf("[string] path: %s\n", res)
@@ -62,4 +73,54 @@ func (w *walker) walkStruct(tt reflect.Type, path string) {
 			fmt.Printf("[unknown] path: %s\n", fmt.Sprintf("%s.%s", path, f.Name))
 		}
 	}
+}
+
+func (w *walker) generateTable() {
+	for _, path := range w.paths {
+		nodes := strings.Split(path, ".")
+		c, _ := strconv.Atoi(nodes[0])
+		p := reflect.ValueOf(&w.domains[c].d)
+		value := p.Elem()
+		fmt.Printf("[path] %s - value of domain: %v, with type: %s, typeOf: %s\n", path, value, value.Type(), reflect.TypeOf(value.Interface()))
+
+		t := reflect.TypeOf(value.Interface())
+		fmt.Printf("%v", t.Field(0))
+
+		v := value
+		if len(nodes) > 2 {
+			for i := 2; i < len(nodes); i++ {
+				fmt.Printf(">> level: %d, node: %s\n", i, nodes[i])
+				var vv reflect.Value
+				if strings.HasPrefix(nodes[i], "[]") {
+					fmt.Printf(">> slice\n")
+					// Slice
+					name := strings.TrimPrefix(nodes[i], "[]")
+					if t.FieldByName(name).Len() == 0 {
+						continue
+					}
+
+					vv = t.FieldByName(name).Index(0)
+				} else {
+					vv = t.FieldByName(nodes[i])
+				}
+
+				fmt.Printf(">> value of field: %v, with type: %s\n", vv, vv.Type())
+				v = vv
+			}
+		}
+
+		if v.Kind() == reflect.String {
+			v.SetString("")
+		}
+
+		// payload, err := json.Marshal(newRoot.Interface())
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// fmt.Printf("[%s][Test] request payload: %s\n", path, string(payload))
+	}
+}
+
+func testStringTags(tags []string) {
+
 }
